9a9e7382623c19a94fdf3725a396642157b210a5
Ian Mckay <contact@ianmckay.com.au>
2025-01-24 13:32:38 +1100

    Update drivedl.py

--- drivedl/drivedl.py
+++ drivedl/drivedl.py
@@ -45,7 +45,12 @@ def get_service(tokenfile):
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
+            try:
+                creds.refresh(Request())
+            except:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
